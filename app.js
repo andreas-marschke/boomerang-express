47c98ac3fda79367964f4b58b12631209256257a
Andreas Marschke <andreas.marschke@gmail.com>
2014-10-03 04:07:57 +0200

    Lots has changed and lots need context...
     # config/master.json
    
     - migrate definition of Numbers to a configurable field in master.json
       Boomerang.js is capable of sending a large variety of datapoints as
       strings and numbers due to "Quirks" in the way nodejs and mongodb
       handles numeric values it is advisable to filter out parameters that
       are numeric in nature and should be casted to be such during the beacon
       import state. Should a boomerang user see the need to adjust the list
       he may do so using an array in config/master.json
    
     # app.js
    
     - _Use a more advanced logging mechanic leveraging bunyan:_ Given the size
       and quantity of requests and data generated by boomerang-express it was
       necessary to have a structured logging mechanism to allow generated data
       to be moved off of the log directory of the server and into a logserver
       through an applicable protocol.
    
     - _Migrate from express.js v3.x to v4 API_ due to the changes in the way
       will handle middleware in the future application initialization has been
       mostly rewritten to reflect these changes.
    
     # lib/routes/index.js: app.any(*)
    
     - _split off Resource Timing data_ into its own collection (resources)
       Using the Resource Timing API in modern browsers and the "restiming.js"
       plugin in boomerang.js a beacon may also contain data referring to
       retrieval timing of other assets on a page. To ease use of these datapoints
       by analysis tools I split off datapoints referring to assets. A reference to
       their unique beacon and subsequent page load timing data is however kept.
    
       For more information on Resource Timing APIs the reader is asked to refer
       to the W3Cs documentation of the Resource Timing API standard:
    
         http://www.w3.org/TR/resource-timing/
    
     - _Support POST based Beacons:_ A previously overlooked case for beacon retrieval
       is the case where beacons are sent using POST-Requests as the datapoints collected
       by boomerang and plugins has exceeded the maximum number of characters in a URL.
    
     - _Support clicks.js plugin:_ allow handling beacons sent using clicks as they are
        different to standard beacon data sent after page.load.
    
     # lib/routes/index.js: mangleQuery()
    
     - _Anonymize IPs retrieved through beacon request-IPs:_ Due to the requirements in
       jurisdictions such as Germany hosters are required to anonymize analytics data
       so as to not pin point data to an explicit request or person for their personal
       safetys sake.

--- app.js
+++ app.js
@@ -5,12 +5,18 @@ var MongoClient = require('mongodb').MongoClient,
     http = require('http'),
     https = require('https'),
     fs = require('fs'),
-    path = require('path')
+    path = require('path'),
+    bodyParser = require("body-parser"),
+    cookieParser = require("cookie-parser"),
+    cookieSession =  require("cookie-session"),
+    bunyan = require('bunyan'),
+    Static = require('serve-static'),
     config = conf.load(process.env.NODE_ENV);
 
 // combine all the servers listed in configuration into one big server-url
 var buildMongoUrl = function(db,servers,credentials,options) {
-    var url = "mongodb://" + credentials.user  + ":" + credentials.password  + "@";
+    var url = "mongodb://";
+    //var url = "mongodb://" + credentials.user  + ":" + credentials.password  + "@";
     for (var i = 0; i < servers.length; i++) {
 	url += servers[i].host + ":" + (servers[i].port || "27017");
 	if (i !== (servers.length - 1)) {
@@ -55,24 +61,73 @@ MongoClient.connect(url,{
     var express = require('express');
     var app = express();
 
-    app.use(express.urlencoded());
-    app.use(express.json());
-    app.use(express.cookieParser('boomerang-express'));
-    app.use(express.logger());
-    app.use(express.cookieSession());
-    app.use(express.static('public'))
+    app.use(Static('public'));
+
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.text({ extended: true, inflate: true })); 
+    app.use(bodyParser.json({ strict: true , type: "text/plain"})); 
+
+    app.use(cookieParser('boomerangexpress'));
+    app.use(cookieSession({ 
+	name: "boomerangexpress",
+	key: "boomerangexpress",
+	keys: ["boomerangexpress"],
+	secret: "boomerangexpress"
+    }));
+    
+    app.use(require('express-bunyan-logger')({
+	name: "boomerang-express",
+	immediate: false,
+	parseUA: true,
+	serializers: {
+	    req: function (req) { 
+		return { 
+		    method: req.method,
+		    url: req.url,
+		    headers: req.headers
+		};
+	    },
+	    res: function () {
+		return null;
+	    },
+	    "user-agent": function (ua) {
+		return { 
+		    browser: ua.family,
+		    version: [ua.major,ua.minor,ua.patch].join("."),
+		    device: ua.device.family,
+		    os: ua.os.family
+		};
+	    },
+	    body: function () { return null; },
+	    "req-headers":  function () { return null; }
+	},
+	streams: [{
+	    stream: process.stdout,
+	    level: 'info'	    
+	}]
+    }));
+
     app.settings.db = db;
+    app.settings.data = config.data;
+    app.settings.logger = bunyan.createLogger({
+	name: "boomerang-express",
+	immediate: true,
+	streams: [{
+	    stream: process.stdout,
+	    level: 'debug'
+	}]
+    });
 
     var routes = require('./lib/routes');
     app.use(routes);
-
+    
     for ( var i = 0; i < config.server.listeners.length; i++) {
 	if (config.server.listeners[i].protocol === "http" ) {
 	    var server = http.createServer(app);
 	    server.listen(
 		config.server.listeners[i].port,
 		config.server.listeners[i].listen,
-		function(){ console.log("HTTP-Server up!"); });
+		function() { app.settings.logger.info("HTTP-Server up!"); });
 	} else if (config.server.listeners[i].protocol === "https" ) {
 
 	    var server = https.createServer({
@@ -83,12 +138,8 @@ MongoClient.connect(url,{
 	    server.listen(
 		config.server.listeners[i].port,
 		config.server.listeners[i].listen,
-		function(){
-		    console.log("HTTPS-Server up!");
-		});
+		function(){ app.settings.logger.info("HTTPS-Server up!"); });
 	}
     }
 });
-
-
 
