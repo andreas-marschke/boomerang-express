7b628a8bf342d748ae3dbcb17bdfd4c93498c865
Andreas Marschke <andreas.marschke@gmail.com>
2014-11-09 06:07:11 +0100

    rewrite internals and backends for better readabillity
    This internal rewrite is based upon recommendations
    documented in the joyent best practices guide.
    
    In addition to this new functionality had been added to
    support full multi-tenancy with multiple sites that are
    meant to be monitored. Meaning: Should you have more than
    one customer you wish to monitor you may configure
    boomerang this way in the backend. Documentation for this
    will be added in the near future.
    
    Also a security enhancement has been made where uid and gid
    can be set using master.json and configure your uid/gid
    this way.
    
    Useragents will be properly analysed now using the useragent
    module from npm.

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ var conf = require('node-conf'),
     path = require('path'),
     config = conf.load(process.env.NODE_ENV),
     bunyan = require('bunyan'),
+    express = require('express'),
+    app = express(),
+    Filters = require('./lib/filters'),
+    Middlewares = require ('./lib/middlewares'),
     Backends = require('./lib/backends');
 
 // combine all the servers listed in configuration into one big server-url
@@ -15,81 +19,86 @@ if ( typeof config.server === "undefined" ) {
     return;
 }
 
-var logger = bunyan.createLogger({
+var streams = [{
+    stream: process.stdout,
+    level: 'debug'
+}];
+
+var datastoreLogger = bunyan.createLogger({
     name: "boomerang-express-datastore",
     immediate: true,
-    streams: [{
-	stream: process.stdout,
-	level: 'debug'
-    }]
+    streams: streams
 });
 
-var ds = new Backends(config.datastore, logger).on("open",function(){
+var filterLogger = bunyan.createLogger({
+    name: "boomerang-express-filter",
+    immediate: true,
+    streams: streams
+});
 
-    var express = require('express');
-    var app = express();
+var appLogger = bunyan.createLogger({
+    name: "boomerang-express-application",
+    immediate: false,
+    streams: streams
+});
 
-    var Middlewares = require ('./lib/middlewares');
+
+var ds = new Backends(config.datastore, datastoreLogger).on("open",main)
+    .on('dbOpenError',handleError)
+    .on('error',handleError);
+
+function handleError(error) {
+
+    datastoreLogger.error( error );
+    process.exit();
+};
+
+function main() {
     new Middlewares(config,app);
+    var filter = new Filters(config.data,filterLogger);
 
-    app.settings.ds = ds;
-    app.settings.log = bunyan.createLogger({
-	name: "boomerang-express-application",
-	immediate: false,
-	streams: [{
-	    stream: process.stdout,
-	    level: 'debug'
-	}]
-    });
-
-    app.settings.data = config.data;
+    app.settings.ds = ds,
+    app.settings.log = appLogger,
+    app.settings.filter = filter;
 
     var routes = require('./lib/routes');
     app.use(routes);
 
-    for ( var i = 0; i < config.server.listeners.length; i++) {
-	if (config.server.listeners[i].protocol === "http" ) {
+    config.server.listeners.forEach(startListener,app);
+};
 
-	    var x = i;
-	    var server = http.createServer(app);
-	    server.listen(
-		config.server.listeners[i].port,
-		config.server.listeners[i].listen,
-		function() {
-		    app.settings.log.info({
-			message: "HTTP-Server up!",
-			port: config.server.listeners[x].port,
-			ip: config.server.listeners[x].listen
-		    });
-		});
-	} else if (config.server.listeners[i].protocol === "https" ) {
-
-	    var p = i;
-
-	    var server = https.createServer({
-		key: fs.readFileSync(path.resolve(config.server.listeners[i].key)),
-		cert: fs.readFileSync(path.resolve(config.server.listeners[i].cert))
-	    },app);
-
-	    server.listen(
-		config.server.listeners[i].port,
-		config.server.listeners[i].listen,
-		function(){
-		    app.settings.log.info({
-			message: "HTTPS-Server up!",
-			port: config.server.listeners[p].port,
-			ip: config.server.listeners[p].listen,
-			key: config.server.listeners[p].key,
-			cert: config.server.listeners[p].cert
-		    });
-		});
-	}
+function postStartup(data) {
+    app.settings.log.info("Server Started");
+    if(typeof config.security !== "undefined") {
+	app.settings.log.info({context: config.security},"Dropping to security context " + (config.security.user || "boomerang") + ":" + (config.security.group || "boomerang"));
+	process.setuid(config.security.user || "boomerang");
+	process.setgid(config.security.group || "boomerang");
+    } else {
+	app.settings.log.info({context: { user: "boomerang", group: "boomerang" }},"Dropping to security context boomerang:boomerang");
+	process.setuid("boomerang");
+	process.setgid("boomerang");
     }
-}).on('dbOpenError',function(exception) {
-    logger.error( exception );
-    process.exit();
-}).on('error',function(error) {
 
-    logger.error( error );
-    process.exit();
-});
+};
+
+function startListener(listener) {
+    if (listener.protocol === "http" ) {
+	httpListener(listener,this);
+    } else if (listener.protocol === "https" ) {
+	httpsListener(listener,this);
+    }
+};
+
+function httpListener (listener, application) {
+    var server = http.createServer(application);
+    server.listen(listener.port, listener.listen, postStartup);
+};
+
+function httpsListener (listener, application) {
+    var server = https.createServer({
+	key: fs.readFileSync(path.resolve(listener.key)),
+	cert: fs.readFileSync(path.resolve(listener.cert))
+    },application);
+
+    server.listen(listener.port, listener.listen, postStartup);
+};
